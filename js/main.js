82d68f7e293766385b526217f0e866d1197ec316
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-18 13:23:13 -0500

    Add Do-While loop nested with if-else to control the flow and filter the correct name.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 //1. Create a prompt to enter name and match it to the correct one for 5x only.
+let correctName = 'Adonis';
+  let yourName;
+  let counter = 5;
+
+  do {
+    yourName = prompt('Enter your name');
+    counter -= 1;
+    
+    if (counter == 0) {
+      console.log('You are not allowed to access it.');
+    } else if (yourName !==correctName){
+      console.log(`${yourName} is not correct. You have ${counter} left`)
+    }else if (yourName === correctName){
+      console.log(`Welcome back, ${correctName} `);
+    }
+  } while (yourName !== correctName && counter !== 0)
