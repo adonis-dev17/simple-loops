59984af10d3feba5505ac5cc3956f398059ef63b
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-19 19:20:32 +0800

    Add clever solution.

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ for (let element = '#'; element.length < 8; element +='#') {
     console.log(output || i);
   }
 
-  // Write a program that creates a string that represents an 8×8 grid, using newline 
+  // 4. Write a program that creates a string that represents an 8×8 grid, using newline 
 //   characters to separate lines. At each position of the grid there is either a space or 
 //   a "#" character. The characters should form a chessboard.
 //   Passing this string to console.log should show something like this:
@@ -81,10 +81,28 @@ for (let element = '#'; element.length < 8; element +='#') {
 // # # # #
 // # # # #
 
+// Solution:
   for (let i=1; i<=4; i++) {
     console.log(' # # # #');
     for (let j=1; j<2;j++) {
      console.log('# # # # ');
     }
-    
   }
+
+  // Clever
+  let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
