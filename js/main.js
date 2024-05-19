6a57551ada7c2f310dc2c2041a20dc041c122451
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-19 13:34:07 +0800

    Add clever solution for Problem2.

--- js/main.js
+++ js/main.js
@@ -58,3 +58,11 @@ for (let element = '#'; element.length < 8; element +='#') {
       console.log(i);
     }
   }
+
+  //Clever:
+  for(let i=1; i<=100; i++) {
+    let output = '';
+    if (i%3 === 0) output += 'Fizz';
+    if (i%5 === 0) output += 'Buzz';
+    console.log(output || i);
+  }
